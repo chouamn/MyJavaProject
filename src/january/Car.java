15ac65e1035a929820df7181cb47e89016442e56
chouamn <chouman.dzuk@gmail.com>
2020-02-02 11:55:43 -0500

    first commit

---
+++ src/january/Car.java
@@ -0,0 +1,5 @@
+package january;
+
+public class Car {
+
+}
---
+++ src/january/main.java
@@ -0,0 +1,23 @@
+package january;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x = 4;
+		int b = 5; 
+		 if (x==b) 
+		  System.out.print("ok");
+		 else 
+		 System.out.print("not ok");
+		
+		
+		
+		
+
+	
+	}
+}
+
